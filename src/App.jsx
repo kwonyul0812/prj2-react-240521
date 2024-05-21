5dc77167e7093f67fc20290d72c05e2cea5d9dc6
kwonyul <kw990812@gmail.com>
2024-05-21 14:20:12 +0900

    게시물 목록 보기 화면

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Home } from "./page/Home.jsx";
-import { BoardWrite } from "./page/board/BoardWrite.jsx";
+import {ChakraProvider} from "@chakra-ui/react";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {Home} from "./page/Home.jsx";
+import {BoardWrite} from "./page/board/BoardWrite.jsx";
+import {BoardList} from "./page/board/BoardList.jsx";
 
 function App(props) {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
-      children: [{ path: "write", element: <BoardWrite /> }],
+      element: <Home/>,
+      children: [
+        {
+          index: true,
+          element: <BoardList/>
+        }, {path: "write", element: <BoardWrite/>}],
     },
   ]);
 
   return (
     <ChakraProvider>
-      <RouterProvider router={router} />
+      <RouterProvider router={router}/>
     </ChakraProvider>
   );
 }
---
+++ src/page/board/BoardList.jsx
@@ -0,0 +1,35 @@
+import {Box, Table, Tbody, Td, Th, Thead, Tr} from "@chakra-ui/react";
+import React, {useState} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faUserPen} from "@fortawesome/free-solid-svg-icons";
+
+export function BoardList() {
+  const [boardList, setBoardList] = useState([]);
+
+  // [{id: 5, title: "제목1", writer: "누구1"},
+  // {id: 5, title: "제목1", writer: "누구1"},
+  // {id: 5, title: "제목1", writer: "누구1"}]
+
+  return <Box>
+    <Box>게시물 목록</Box>
+    <Box>
+      <Table>
+        <Thead>
+          <Tr>
+            <Th>#</Th>
+            <Th>TITLE</Th>
+            <Th><FontAwesomeIcon icon={faUserPen}/></Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {boardList.map((board) =>
+            <Tr key={board.id}>
+              <Td>{board.id}</Td>
+              <Td>{board.title}</Td>
+              <Td>{board.writer}</Td>
+            </Tr>)}
+        </Tbody>
+      </Table>
+    </Box>
+  </Box>;
+}
