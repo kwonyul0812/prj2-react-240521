be99889f9f32b4883ed2971b2a8a20ca433a6435
kwonyul <kw990812@gmail.com>
2024-05-21 16:17:24 +0900

    수정 요청

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import {Home} from "./page/Home.jsx";
 import {BoardWrite} from "./page/board/BoardWrite.jsx";
 import {BoardList} from "./page/board/BoardList.jsx";
 import {BoardView} from "./page/board/BoardView.jsx";
+import {BoardEdit} from "./page/board/BoardEdit.jsx";
 
 function App(props) {
   const router = createBrowserRouter([
@@ -17,7 +18,8 @@ function App(props) {
           element: <BoardList/>
         },
         {path: "write", element: <BoardWrite/>},
-        {path: "board/:id", element: <BoardView/>}],
+        {path: "board/:id", element: <BoardView/>},
+        {path: "edit/:id", element: <BoardEdit/>}],
     },
   ]);
 
