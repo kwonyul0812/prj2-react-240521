b88234b5fbeffd958d2d808f33c6f35452c36f18
kwonyul <kw990812@gmail.com>
2024-05-21 12:20:08 +0900

    글 등록 성공시 toast 출력, HOME으로 이동

--- src/page/board/BoardWrite.jsx
+++ src/page/board/BoardWrite.jsx
@@ -4,22 +4,34 @@ import {
   FormControl,
   FormLabel,
   Input,
-  Textarea,
+  Textarea, useToast,
 } from "@chakra-ui/react";
-import React, { useState } from "react";
+import React, {useState} from "react";
 import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 export function BoardWrite() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [writer, setWriter] = useState("");
+  const toast = useToast();
+  const navigate = useNavigate();
 
   function handleSaveClick() {
     axios.post("/api/board/add", {
       title,
       content,
       writer,
-    });
+    }).then(() => {
+      toast({
+        description: "새 글이 등록되었습니다.",
+        status: "success",
+        position: "top",
+      })
+      navigate("/")
+    })
+      .catch()
+      .finally();
   }
 
   return (
@@ -29,19 +41,19 @@ export function BoardWrite() {
         <Box>
           <FormControl>
             <FormLabel>제목</FormLabel>
-            <Input onChange={(e) => setTitle(e.target.value)} />
+            <Input onChange={(e) => setTitle(e.target.value)}/>
           </FormControl>
         </Box>
         <Box>
           <FormControl>
             <FormLabel>본문</FormLabel>
-            <Textarea onChange={(e) => setContent(e.target.value)} />
+            <Textarea onChange={(e) => setContent(e.target.value)}/>
           </FormControl>
         </Box>
         <Box>
           <FormControl>
             <FormLabel>작성자</FormLabel>
-            <Input onChange={(e) => setWriter(e.target.value)} />
+            <Input onChange={(e) => setWriter(e.target.value)}/>
           </FormControl>
         </Box>
         <Box>
