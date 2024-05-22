c27cf55cde688885cabbc732854e646df3a1ba91
kwonyul <kw990812@gmail.com>
2024-05-22 11:32:45 +0900

    이메일,별명 중복확인 안되면 안내 메세지 출력

--- src/page/member/MemberSignup.jsx
+++ src/page/member/MemberSignup.jsx
@@ -153,6 +153,9 @@ export function MemberSignup() {
                 </Button>
               </InputRightElement>
             </InputGroup>
+            {isCheckedEmail || (
+              <FormHelperText>이메일 중복확인을 해주세요.</FormHelperText>
+            )}
           </FormControl>
         </Box>
         <Box>
@@ -186,6 +189,9 @@ export function MemberSignup() {
                 </Button>
               </InputRightElement>
             </InputGroup>
+            {isCheckedNickName || (
+              <FormHelperText>별명 중복 확인을 해주세요.</FormHelperText>
+            )}
           </FormControl>
         </Box>
         <Box>
