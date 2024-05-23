477dbf6abc5db0c1aae39956fa9b60d57b2ab919
kwonyul <kw990812@gmail.com>
2024-05-23 09:48:51 +0900

    회원 정보 변경

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { BoardEdit } from "./page/board/BoardEdit.jsx";
 import { MemberSignup } from "./page/member/MemberSignup.jsx";
 import { MemberList } from "./page/member/MemberList.jsx";
 import { MemberInfo } from "./page/member/MemberInfo.jsx";
+import { MemberEdit } from "./page/member/MemberEdit.jsx";
 
 function App(props) {
   const router = createBrowserRouter([
@@ -26,6 +27,7 @@ function App(props) {
         { path: "signup", element: <MemberSignup /> },
         { path: "member/list", element: <MemberList /> },
         { path: "member/:id", element: <MemberInfo /> },
+        { path: "member/edit/:id", element: <MemberEdit /> },
       ],
     },
   ]);
