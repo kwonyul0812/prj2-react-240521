d4f3200fb4f147e154ce8f261ea5a9ef27a7a198
kwonyul <kw990812@gmail.com>
2024-05-27 09:33:00 +0900

    게시물 목록에서 페이지 번호 넘기기

--- src/page/board/BoardList.jsx
+++ src/page/board/BoardList.jsx
@@ -1,41 +1,53 @@
-import {Box, Table, Tbody, Td, Th, Thead, Tr} from "@chakra-ui/react";
-import React, {useEffect, useState} from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faUserPen} from "@fortawesome/free-solid-svg-icons";
+import { Box, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserPen } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
-import {useNavigate} from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 export function BoardList() {
   const [boardList, setBoardList] = useState([]);
   const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
 
   useEffect(() => {
-    axios.get("/api/board/list")
-      .then(res => setBoardList(res.data));
+    axios
+      .get(`/api/board/list?${searchParams}`)
+      .then((res) => setBoardList(res.data));
   }, []);
 
-  return <Box>
-    <Box>게시물 목록</Box>
+  return (
     <Box>
-      <Table>
-        <Thead>
-          <Tr>
-            <Th>#</Th>
-            <Th>TITLE</Th>
-            <Th><FontAwesomeIcon icon={faUserPen}/></Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {boardList.map((board) =>
-            <Tr _hover={{
-              bgColor: "gray.200",
-            }} cursor={"pointer"} onClick={() => navigate(`/board/${board.id}`)} key={board.id}>
-              <Td>{board.id}</Td>
-              <Td>{board.title}</Td>
-              <Td>{board.writer}</Td>
-            </Tr>)}
-        </Tbody>
-      </Table>
+      <Box>게시물 목록</Box>
+      <Box>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>#</Th>
+              <Th>TITLE</Th>
+              <Th>
+                <FontAwesomeIcon icon={faUserPen} />
+              </Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {boardList.map((board) => (
+              <Tr
+                _hover={{
+                  bgColor: "gray.200",
+                }}
+                cursor={"pointer"}
+                onClick={() => navigate(`/board/${board.id}`)}
+                key={board.id}
+              >
+                <Td>{board.id}</Td>
+                <Td>{board.title}</Td>
+                <Td>{board.writer}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </Box>
     </Box>
-  </Box>;
+  );
 }
