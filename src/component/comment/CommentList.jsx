3cd79b865a230f8c7a84c2c5fdb1fe8ae3dee53f
kwonyul <kw990812@gmail.com>
2024-05-30 15:29:24 +0900

    댓글 지우기

--- src/component/comment/CommentList.jsx
+++ src/component/comment/CommentList.jsx
@@ -1,6 +1,7 @@
-import { Box, Flex, Spacer } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { CommentItem } from "./CommentItem.jsx";
 
 export function CommentList({ boardId, isSending }) {
   const [commentList, setCommentList] = useState([]);
@@ -22,14 +23,7 @@ export function CommentList({ boardId, isSending }) {
   return (
     <Box>
       {commentList.map((comment) => (
-        <Box key={comment.id} border={"1px solid black"} my={3}>
-          <Flex>
-            <Box>{comment.nickName}</Box>
-            <Spacer />
-            <Box>{comment.inserted}</Box>
-          </Flex>
-          <Box>{comment.comment}</Box>
-        </Box>
+        <CommentItem comment={comment} key={comment.id} />
       ))}
     </Box>
   );
